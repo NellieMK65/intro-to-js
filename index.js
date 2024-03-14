6349ff75e44aaf05163c26322d92aebb1be60cfe
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-03-14 11:28:46 +0300

    Intro to js & variables

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log(3);
+
+console.log(34 + 23);
+
+console.log(3 * (10 - 4));
+
+console.log('Nelson');
+
+// Assignment Expression
+age = 30;
+sum = 5 + 5;
+
+console.log(age);
+
+console.log(sum);
---
+++ variable.js
@@ -0,0 +1,28 @@
+// Variable - a container that can hold values for later retrieval
+
+// Declaring
+// 1. const - immutable (value cannot change)
+const school = 'Moringa School';
+
+console.log(school);
+
+// school = 'KU';
+// 2. let - mutable (value can change)
+let age = 45;
+
+console.log(age);
+
+console.log(age);
+
+// reassigning a new value
+age = 34;
+
+console.log(age);
+
+let fullName = 'John Doe';
+
+console.log(fullName);
+
+fullName = 'Jane Doe';
+
+console.log(fullName);
